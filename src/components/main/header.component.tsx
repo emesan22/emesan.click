82c7970748e890353d57104c645918838e481f7e
emesan <emesan@mail.emesan.click>
2024-02-04 13:35:01 +0900

    :art: レポンスの修正

--- src/components/main/header.component.tsx
+++ src/components/main/header.component.tsx
@@ -27,16 +27,18 @@ const navigationItems: NavigationItem[] = [
 export default function Header() {
   return (
     <header className="fixed w-full bg-background text-text">
-      <div className="flex flex-wrap justify-between items-center border-b-2 border-solid border-b-primary mx-auto px-5 h-14">
-        <h1 className="font-bold text-2xl">蒼氷えめさん</h1>
-        <nav className="h-full">
+      <div className="flex flex-wrap justify-center xl:justify-between items-center border-b-2 border-solid border-b-primary mx-auto px-2 md:px-5 h-[5rem] xl:h-12">
+        <h1 className="font-bold text-2xl text-center xl:text-left">
+          蒼氷えめさん
+        </h1>
+        <nav className="">
           <ul className="flex flex-wrap h-full align-center">
             {navigationItems.map((item) => (
-              <li
-                key={item.label}
-                className="h-full hover:bg-gray-200 dark:hover:bg-gray-600 transition-all duration-300 ease-in-out"
-              >
-                <Link href={item.href} className="block h-full px-5 mt-3 ">
+              <li key={item.label} className="">
+                <Link
+                  href={item.href}
+                  className="block h-[5rem] xl:h-[3rem] pt-7 xl:pt-3 px-2 md:px-5 hover:bg-gray-200 dark:hover:bg-gray-600 transition-all duration-300 ease-in-out"
+                >
                   {item.label}
                 </Link>
               </li>
